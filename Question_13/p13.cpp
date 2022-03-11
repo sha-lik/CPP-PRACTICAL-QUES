f7999bd012ec1c466b487f70adb6c183a539bce2
SHANKAR <98207888+sha-lik@users.noreply.github.com>
2022-03-11 13:09:14 +0530

    Update p13.cpp

--- Question_13/p13.cpp
+++ Question_13/p13.cpp
@@ -1,10 +1,11 @@
 /*
-13. Create a class Student containing fields for Roll No., Name, Class, Year and Total
-Marks. Write a program to store 5 objects of Student class in a file. Retrieve these records
-from file and display them.
+2. Create a class Student containing fields for Roll No., Name, Class, Year and Total Marks. Write a program to store 5 objects of Student class in a file. Retrieve these records from file and display them.
 */
 
-#include <bits/stdc++.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
 using namespace std;
 
 class Student
@@ -19,25 +20,49 @@ public:
 
 int main()
 {
-    ofstream f2;
-    f2.open("Question_13_output.txt");
-
-    for (int i = 0; i < 5; i++)
+    ofstream f1;
+    f1.open("Ques_2_output.txt");
+    for (int i = 1; i < 6; i++)
     {
+        cout << "Enter details of Student : " << i << endl;
         Student obj;
-
+        f1 << "Details of Student : " << i << endl;
+        cout << "Roll No     : ";
         cin >> obj.RollNo;
-        f2 << obj.RollNo << "  ";
+        f1 << "Roll No     : " << obj.RollNo << endl;
 
+        cout << "Name        : ";
         cin.ignore();
         getline(cin, obj.Name);
-        f2 << obj.Name << "    ";
+        f1 << "Name        :" << obj.Name << endl;
 
-        cin >> obj.Class >> obj.Year >> obj.TotalMarks;
-        f2 << obj.Class << "  " << obj.Year << "  " << obj.TotalMarks << endl;
+        cout << "Class       : ";
+        cin >> obj.Class;
+        f1 << "Class       : " << obj.Class << endl;
 
-        cout << "\n---------------------------------\n";
+        cout << "Year        : ";
+        cin >> obj.Year;
+        f1 << "Year        : " << obj.Year << endl;
+
+        cout << "Total Marks : ";
+        cin >> obj.TotalMarks;
+        cout << endl
+             << endl;
+
+        f1 << "Total Marks : " << obj.TotalMarks << endl
+           << endl;
+
+        cout << "\n---------------------------\n";
     }
+    f1.close();
 
+    ifstream f2;
+    f2.open("Ques_2_output.txt");
+    while (f2.good())
+    {
+        string str;
+        getline(f2, str);
+        cout << str << endl;
+    }
     return 0;
 }
